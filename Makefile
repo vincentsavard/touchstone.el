d02877db4cf44063496cacb9b42fe501434b3e13
Vincent Savard <savard.vincent@gmail.com>
2025-11-11 14:16:32 -0500

    core: test the output

--- Makefile
+++ Makefile
@@ -5,7 +5,8 @@ EMACS ?= emacs
 TEST_HELPER = tests/test-helper.el
 TEST_FILE = tests/touchstone-test.el
 PYTEST_TEST_FILE = tests/touchstone-pytest-test.el
+CORE_TEST_FILE = tests/touchstone-core-test.el
 
 .PHONY: test
 test:
-	$(EMACS) -batch -l $(TEST_HELPER) -l $(TEST_FILE) -l $(PYTEST_TEST_FILE) -f ert-run-tests-batch-and-exit
+	$(EMACS) -batch -l $(TEST_HELPER) -l $(TEST_FILE) -l $(PYTEST_TEST_FILE) -l $(CORE_TEST_FILE) -f ert-run-tests-batch-and-exit
---
+++ tests/touchstone-core-test.el
@@ -0,0 +1,72 @@
+;;; touchstone-core-test.el --- Tests for touchstone.el core -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Isolated unit tests for the core using a fake backend
+
+;;; Code:
+
+(require 'ert)
+(require 'test-helper)
+
+;;; Fake Backend Implementation
+
+(defun touchstone-core-test-create-fake-backend (results)
+  "Create a simple fake backend for testing core.
+RESULTS is a list of test result plists to return."
+  (list
+   :name "fake"
+   :detect (lambda (root) t)
+   :build-command (lambda (root)
+                    (let ((n (length results)))
+                      (list "printf" (concat (make-string n ?\n)))))
+   :create-parser-state (lambda () (list :results results))
+   :parse-line (lambda (line state)
+                 (let ((results (plist-get state :results)))
+                   (if results
+                       (let ((result (car results))
+                             (remaining (cdr results)))
+                         (cons result (plist-put state :results remaining)))
+                     (cons nil state))))
+   :finish (lambda (state) (cons nil state))))
+
+;;; Helper Functions
+
+(defun touchstone-core-test-register-fake-backend (results)
+  "Register a fake backend that returns RESULTS."
+  (let ((backend (touchstone-core-test-create-fake-backend results)))
+    (touchstone-register-backend 'fake backend)
+    backend))
+
+(defun touchstone-core-test-wait-for-process ()
+  "Wait for the touchstone process to complete."
+  (when-let ((proc (get-buffer-process (get-buffer " *touchstone-process*"))))
+    (while (eq (process-status proc) 'run)
+      (accept-process-output proc 0.1))))
+
+(defun touchstone-core-test-get-buffer-text ()
+  "Get buffer text without properties."
+  (with-current-buffer (get-buffer touchstone-buffer-name)
+    (buffer-substring-no-properties (point-min) (point-max))))
+
+(defun touchstone-core-test-buffer-contains (text)
+  "Return t if buffer contains TEXT."
+  (string-match-p (regexp-quote text) (touchstone-core-test-get-buffer-text)))
+
+;;; Tests
+
+(ert-deftest touchstone-core-test-display-single-passed-test ()
+  "Test that a single passing test is displayed correctly."
+  (touchstone-core-test-register-fake-backend
+   (list (list :id "file.txt::test_one"
+               :file "file.txt"
+               :test "test_one"
+               :status 'passed)))
+
+  (touchstone-run-tests)
+  (touchstone-core-test-wait-for-process)
+
+  (should (touchstone-core-test-buffer-contains "PASS  file.txt::test_one")))
+
+(provide 'touchstone-core-test)
+
+;;; touchstone-core-test.el ends here
